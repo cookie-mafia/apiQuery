5a65a57a416d893d3992a22a24ce7ded62181f6a
Joseph Panuncillo <jpanuncillo@live.com>
2016-09-12 06:50:49 +0800

    chore: setup unit test

---
+++ src/index.test.js
@@ -0,0 +1,11 @@
+'use strict';
+
+require( 'should' );
+
+const apiQuery = require( './index.js' );
+
+describe( 'attributes', () => {
+	it( 'version check', () => {
+		apiQuery.should.have.property('version');
+	} );
+} );
