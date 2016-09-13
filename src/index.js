df944952263f1bb9b8347097402f824b46be55b9
nakautot <jpanuncillo@live.com>
2016-09-13 13:05:10 +0800

    feature: add ability to set actionProviders

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 'use strict';
 
+function setActionProvider(actionProvider) {
+  this.actions = actionProvider;
+}
+
+function start(overriderActionProvider) {
+  let actions = overriderActionProvider || this.actions;
+
+  return actions.execute();
+}
+
 module.exports = {
-  'version': '1.0.0'
+  'version': '1.0.0',
+  'actions': require('./defaultActionProvider.js'),
+  setActionProvider,
+  start
 };
--- src/index.test.js
+++ src/index.test.js
@@ -2,10 +2,32 @@
 
 require('should');
 
-const apiQuery = require('./index.js');
+const apiQuery        = require('./index.js');
+const testProviderStr = 'dummy action provider';
+const testProvider    = {
+  execute() {
+    return testProviderStr;
+  }
+};
 
 describe('attributes', () => {
   it('version check', () => {
     apiQuery.should.have.property('version');
+  });
+});
+
+describe('methods', () => {
+  it('should be able to register a new actionProvider', () => {
+    apiQuery.setActionProvider(testProviderStr);
+    apiQuery.actions.should.equal(testProviderStr);
+  });
+
+  it('should be able to override actionProvider on apiQuery.start', () => {
+    apiQuery.start(testProvider).should.equal(testProviderStr);
+  });
+
+  it('should be able to use last actionProvider set on apiQuery.start', () => {
+    apiQuery.setActionProvider(testProvider);
+    apiQuery.start().should.equal(testProviderStr);
   });
 });
